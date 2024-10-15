719f2a5d9a6cc2774a9e968a6398882786af1ba6
NureNevmovenkoVsevolod <vsevolod.nevmovenko@nure.ua>
2024-10-15 12:03:25 +0200

    Create MainActivity.java

---
+++ PractTask1/android-pzpi-23-8-nevmovenko-vsevolod-pract-task-1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.a1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Новий код
+        TextView textView = findViewById(R.id.textView);  // Пошук TextView по ID
+        Button button = findViewById(R.id.button);  // Пошук Button по ID
+
+        // Обробка натискання кнопки
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Зміна тексту при натисканні
+                textView.setText("Текст змінився");
+            }
+        });
+    }
+}
